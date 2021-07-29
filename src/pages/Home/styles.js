e7ef891e2b110f1397c81d5ff74a2abd37a07ebe
Alexandre Julião <alexand.juliao@gmail.com>
2021-07-29 05:20:14 -0300

    create projeto and reset style

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  /* background: ${(props) => props.theme.colors.primary}; */
+  `
+
+export default Container;
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    primary: '#6200EE',
+    background: '#000014',
+    text: '#000083',
+    sucess: '#135E01',
+    warning: '#FFCB1F',
+    error: '#FF0000'
+  },
+  fonst: {
+    regular: 'Roboto, sans-serif',
+  },
+
+}
+
+export default theme;
